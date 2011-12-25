cf1ecc02fb501de9537f2e6d676125dbe5f4e49c
benoitc <bchesneau@gmail.com>
2011-12-25 01:24:10 +0100

    remove the prefix t%2F from the food url.

--- shows/random.js
+++ shows/random.js
@@ -1,18 +1,33 @@
 function(doc, req) {
     var ddoc = this;
 
-    if (!doc) {
-        return "please reload";
-    }
-
     var Mustache = require("lib/mustache"),
         path = require("lib/path").init(req);
 
+    if (!doc) {
+        // old url
+        if (req.id && req.id.match("^t/t/")) {
+            var docid = req.id.split("/")[2];
+            log("reqid: " + req.id);
+            log("docid: " + docid);
+            var redirect_url = path.absolute(path.rewrite() + "/" + docid);
+            log("redirect_url: " + redirect_url);
+            return {
+                code: 301,
+                headers: {"Location": redirect_url},
+                body: "Redirected to " + redirect_url
+            };
+        }
+
+        return "please reload";
+    }
+
 
     var templates = ddoc.templates;
-    var docid = doc._id.replace("/", "%2F");
+    var docid = doc._id.replace("/", "%2F"),
+        href = doc._id.replace("t/", "");
 
-   
+
     var photo_uri = path.rewrite() + "/photo/" + docid;
     var profile_uri  = path.rewrite() + "/avatar/" + docid;
 
@@ -20,15 +35,15 @@ function(doc, req) {
         is_location = false;
     if (doc.source.geo) {
         is_location = true;
-        coords = { 
+        coords = {
             "lon": doc.source.geo.coordinates[0],
             "lat": doc.source.geo.coordinates[1]
         };
     }
 
-    var uri = path.absolute(path.rewrite() + "/" + docid)
+    var uri = path.absolute(path.rewrite() + "/" + href)
 
-    
+
     provides("html", function() {
         return Mustache.to_html(templates.food, {
             doc: doc,
@@ -38,7 +53,7 @@ function(doc, req) {
             vhost: req.headers['x-couchdb-vhost-path'],
             is_location: is_location,
             coords: coords,
-            uri: uri     
+            uri: uri
         });
     });
 }
